cc6f12a4b1b69c5e9445d730af5f356ebe902c40
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 09:57:44 -0300

    Add: arquivos animals e filmes

---
+++ animals.js
@@ -0,0 +1,8 @@
+const animals = ["Macaco", "tubarão", "Crocodilo", "jaguatirica"]
+
+if(!animals.includes("Leão")){
+    console.log("Leão não existe no array animals.")
+}
+else{
+    console.log("Existe um leão no array animals.")
+}
---
+++ filmes.js
@@ -0,0 +1,8 @@
+const my3FavoriteTVShows = ["Alladin e os 40 ladroes", "boyhood", "interestelar"]
+
+if(my3FavoriteTVShows.length === 3){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
